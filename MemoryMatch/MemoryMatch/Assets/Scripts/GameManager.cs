85fb6f8ea0f67778862ea1b3f3ffd5f1025c9410
pnam69 <67112436+pnam69@users.noreply.github.com>
2026-04-17 22:52:01 +0700

    memory match stuff

---
+++ MemoryMatch/MemoryMatch/Assets/Scripts/GameManager.cs
@@ -0,0 +1,154 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int score = 0;
+    public int level = 1;
+    public TMP_Text scoreText;
+    public TMP_Text highScoreText;
+    public TMP_Text levelText;
+    public GameObject gameOverPanel;
+    public GameObject mainMenuPanel;
+    public GameObject pausePanel;
+    public GameObject highScorePanel;
+    public GameObject gameHUDPanel;
+    public GameObject settingsPanel;
+    public Button pauseButton;
+    public Toggle soundToggle;
+    public TMP_Dropdown difficultyDropdown;
+    public bool isStarted = false;
+    public int highScore = 0;
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        difficultyDropdown.value = PlayerPrefs.GetInt("Difficulty", 1);
+        soundToggle.isOn = PlayerPrefs.GetInt("Sound", 1) == 1;
+        AudioListener.volume = soundToggle.isOn ? 1f : 0f;
+        mainMenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+        highScorePanel.SetActive(false);
+        gameHUDPanel.SetActive(false);
+        pausePanel.SetActive(false);
+        gameOverPanel.SetActive(false);
+
+        Time.timeScale = 0f;
+    }
+    void Awake()
+    {
+        Instance = this;
+    }
+    private void Update()
+    {
+        if (isGameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            RestartGame();
+        }
+    }
+    public void StartGame()
+    {
+        isStarted = true;
+        mainMenuPanel.SetActive(false);
+        gameHUDPanel.SetActive(true);
+
+        score = 0;
+        level = 1;
+        isGameOver = false;
+
+        UpdateUI();
+
+        Time.timeScale = 1f;
+    }
+    public void OpenHighScore()
+    {
+        mainMenuPanel.SetActive(false);
+        highScorePanel.SetActive(true);
+
+        highScoreText.text = "High Score: " + highScore;
+    }
+    public void OpenSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+    public void BackToMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+        highScorePanel.SetActive(false);
+    }
+    public void SaveDifficulty()
+    {
+        PlayerPrefs.SetInt("Difficulty", difficultyDropdown.value);
+    }
+    public void SaveSound()
+    {
+        PlayerPrefs.SetInt("Sound", soundToggle.isOn ? 1 : 0);
+        AudioListener.volume = soundToggle.isOn ? 1f : 0f;
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pausePanel.SetActive(true);
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pausePanel.SetActive(false);
+    }
+    public void AddScore(int amount)
+    {
+        score += amount;
+        NextLevel();
+        UpdateUI();
+    }
+    public void UpdateUI()
+    {
+        scoreText.text = "Score: " + score;
+        levelText.text = "Level: " + level;
+    }
+    public void NextLevel()
+    {
+        int newLevel = score / 50 + 1;
+
+        if (newLevel > level)
+        {
+            level = newLevel;
+            Debug.Log("Level Up! " + level);
+        }
+    }
+    public bool isGameOver = false;
+
+    public void GameOver()
+    {
+        isGameOver = true;
+
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("HighScore", highScore);
+        }
+
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void RestartGame()
+    {
+        gameOverPanel.SetActive(false);
+
+        score = 0;
+        level = 1;
+        isGameOver = false;
+
+        UpdateUI();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ MemoryMatch/MemoryMatch/Assets/Scripts/SwipeInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class SwipeInput : MonoBehaviour
+{
+
+    private Vector2 startTouch;
+    private Vector2 endTouch;
+
+    void Update()
+    {
+        if (Input.touchCount == 0) return;
+
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began)
+            startTouch = touch.position;
+
+        if (touch.phase == TouchPhase.Ended)
+        {
+            endTouch = touch.position;
+            DetectSwipe();
+        }
+    }
+
+    void DetectSwipe()
+    {
+        Vector2 delta = endTouch - startTouch;
+
+        if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+        {
+        }
+        else
+        {
+        }
+    }
+}
