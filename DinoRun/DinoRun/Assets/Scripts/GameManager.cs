9e6c4e031980629d17df65e5ef9e628b3b066405
pnam69 <67112436+pnam69@users.noreply.github.com>
2026-05-22 09:48:27 +0700

    add more assets

--- DinoRun/DinoRun/Assets/Scripts/GameManager.cs
+++ DinoRun/DinoRun/Assets/Scripts/GameManager.cs
@@ -51,6 +51,7 @@ public class GameManager : MonoBehaviour
     {
         gameStarted = true;
         isGameOver = false;
+        score = 0;
     }
 
     public void GameOver()
--- DinoRun/DinoRun/Assets/Scripts/UIManager.cs
+++ DinoRun/DinoRun/Assets/Scripts/UIManager.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject startPanel;
     public GameObject gameOverPanel;
 
+    void Awake()
+    {
+        // Ensure there's an EventSystem in the scene, which is required for UI interaction
+        if (EventSystem.current == null && FindObjectOfType<EventSystem>() == null)
+        {
+            GameObject eventSystemInfo = new GameObject("EventSystem");
+            eventSystemInfo.AddComponent<EventSystem>();
+            eventSystemInfo.AddComponent<StandaloneInputModule>();
+        }
+
+        // Ensure the Canvas containing this UI actually has a GraphicRaycaster component
+        Canvas canvas = GetComponentInParent<Canvas>();
+        if (canvas != null && canvas.GetComponent<GraphicRaycaster>() == null)
+        {
+            canvas.gameObject.AddComponent<GraphicRaycaster>();
+        }
+    }
+
     public void StartGame()
     {
-        startPanel.SetActive(false);
-        GameManager.Instance.StartGame();
+        if (startPanel != null) startPanel.SetActive(false);
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.StartGame();
+        }
     }
 
     public void RestartGame()
